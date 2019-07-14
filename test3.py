df193a998fe1805781e104fed277e3939103b58f
weibs <39818440+weibingshan@users.noreply.github.com>
2019-07-14 22:08:17 +0800

    test1

---
+++ test3.py
@@ -0,0 +1,17 @@
+#!/user/bin/env.python
+# -*- coding:utf-8 -*-
+"""
+@author: weibs
+@contact: 110121839@qq.com
+@software: PyCharm
+@file: test3.py
+@time: 2019/7/14 22:07
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
